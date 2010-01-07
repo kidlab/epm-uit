7adcd1b72da2e143e12fb048f4bf5247d5647a4f
thanhman.gm <thanhman.gm@e6454eb4-b7b6-11de-a66b-f3ad5cb19abf>
2010-01-07 17:37:39 +0000

    - Add tiny_mce, new js files... - Change Site.Master - Change Error page.

---
+++ trunk/source_code/EPM/Scripts/mce_advanced.js
@@ -0,0 +1,11 @@
+tinyMCE.init({
+	mode : "textareas",
+	theme : "advanced",
+	width : "100%",
+	theme_advanced_toolbar_location: 'top',
+	theme_advanced_toolbar_align: 'left',
+	theme_advanced_resizing : true,
+	theme_advanced_buttons1 : 'bold, italic, underline, strikethrough, |, justifyleft, justifycenter, justifyright, justifyfull, |, bullist, numlist, |, outdent, indent ,|, fontselect, fontsizeselect, |, cut, copy, paste',
+	theme_advanced_buttons2 : '',
+	theme_advanced_buttons3 : '',	
+});	
