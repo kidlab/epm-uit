5e5bc0857c9e54926fc316b233addd71df48f6f3
thanhman.gm <thanhman.gm@e6454eb4-b7b6-11de-a66b-f3ad5cb19abf>
2010-01-15 16:52:43 +0000

    Prepare for EPMClient

---
+++ trunk/source_code/Common/UIUtils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Common
+{
+    public class UIUtils
+    {
+        /// <summary>
+        /// Shows an error message.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult Error(string msg)
+        {
+            return MessageBox.Show(msg, EpmConst.EPM_AGENT, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Show an information message.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult Info(string msg)
+        {
+            return MessageBox.Show(msg, EpmConst.EPM_AGENT, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Shows a caution message.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult Warning(string msg)
+        {
+            return MessageBox.Show(msg, EpmConst.EPM_AGENT, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        /// <summary>
+        /// Shows a confirm dialog with two options: Yes, No.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult Confirm(string msg)
+        {
+            return MessageBox.Show(msg, EpmConst.EPM_AGENT, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+        /// <summary>
+        /// Shows a confirm dialog with three options: Yes, No and Cancel.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult ConfirmYesNoCancel(string msg)
+        {
+            return MessageBox.Show(msg, EpmConst.EPM_AGENT, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+        }
+
+        /// <summary>
+        /// Shows a confirm dialog with three options: OK and Cancel.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult ConfirmOKCancel(string msg)
+        {
+            return MessageBox.Show(msg, EpmConst.EPM_AGENT, MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+        }
+    }
+}
